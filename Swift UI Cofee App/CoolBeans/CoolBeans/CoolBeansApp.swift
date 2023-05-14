6ac07886b6ca7dd04f35e93cd4701f74ce16e6c8
Irtiza <irtizakh@gmail.com>
2023-05-14 13:24:17 +0500

    Addition - Coffee app project is created

---
+++ Swift UI Cofee App/CoolBeans/CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Muhammad Irtiza Khursheed on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
